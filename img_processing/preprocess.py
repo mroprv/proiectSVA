964593871f30f70453d707c278cc4021cb48b3f7
mroprv <mario-alexandru.pirvan@student.tuiasi.ro>
2026-05-27 16:20:51 +0300

    changed preprocessing to accomodate irl photos

--- img_processing/preprocess.py
+++ img_processing/preprocess.py
@@ -1,19 +1,30 @@
 import cv2
 import numpy as np
+
+from config import IS_PHOTO_REAL
 def preprocess(rgbImage): #preprocess the image
     #binarize
     gray = cv2.cvtColor(rgbImage, cv2.COLOR_BGR2GRAY)
-
-    #apply blur
-    blur = cv2.GaussianBlur(gray, (5, 5), 0)
-
     
-    thresh = cv2.adaptiveThreshold(blur,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY_INV,11,2)
+    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+    gray = clahe.apply(gray)
+
+    bg = cv2.GaussianBlur(gray, (55,55), 0)
+
+    norm = cv2.divide(gray, bg, scale=255)
+
+    blur = cv2.GaussianBlur(norm, (5,5), 0)
+
+    _, thresh = cv2.threshold(blur,0,255,cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+
+    cv2.imwrite("debug.jpg",thresh)
 
     #close and open
     kernel = np.ones((3, 3), np.uint8)
 
     thresh = cv2.morphologyEx(thresh,cv2.MORPH_CLOSE,kernel,iterations=2)
-    thresh = cv2.morphologyEx(thresh,cv2.MORPH_OPEN,kernel,iterations=1)
+    thresh = cv2.morphologyEx(thresh,cv2.MORPH_OPEN,kernel,iterations=1+IS_PHOTO_REAL)
+
+    
 
     return thresh
